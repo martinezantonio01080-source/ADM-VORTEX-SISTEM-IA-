9488acee544fbc3b8f58422c163197fbd3fb23a5
martinezantonio01080-source <martinezantonio01080@gmail.com>
2026-07-31 20:37:51 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 function generateCertificate() {
-    const input = document.getElementById('projectName').value.trim();
+    const projectInput = document.getElementById('projectName').value.trim();
+    const authorInput = document.getElementById('authorName').value.trim();
+    const titleInput = document.getElementById('techTitle').value;
+    
     const cert = document.getElementById('certificate');
     const resName = document.getElementById('resName');
+    const resAuthor = document.getElementById('resAuthor');
+    const resTitle = document.getElementById('resTitle');
     const serialNum = document.getElementById('serialNumber');
     const timestamp = document.getElementById('timestamp');
 
-    if (!input) {
-        alert('Please type the name of your masterpiece first!');
+    if (!projectInput || !authorInput) {
+        alert('Please fill out both text fields, master!');
         return;
     }
 
-    resName.textContent = input;
+    resName.textContent = projectInput;
+    resAuthor.textContent = authorInput;
+    resTitle.textContent = titleInput;
     
     // Generate serial number and current date
     const randomCode = Math.floor(1000 + Math.random() * 9000);
